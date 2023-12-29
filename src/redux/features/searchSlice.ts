0af67358aecf01b903cc657f272ef098adb1c423
Aviv Turgeman <avivturgeman10@gmail.com>
2023-12-29 15:13:00 +0200

    fe-1

---
+++ src/redux/features/searchSlice.ts
@@ -0,0 +1,11 @@
+import { createSlice } from "@reduxjs/toolkit"
+
+const searchSlice = createSlice({
+    name:'search',
+    initialState:false,
+    reducers:{
+
+    }
+})
+
+export default searchSlice;
---
+++ src/redux/store.ts
@@ -0,0 +1,12 @@
+import { configureStore } from '@reduxjs/toolkit';
+import searchSlice from './features/searchSlice';
+
+
+
+const store = configureStore({
+    reducer: {
+        searchSlice,
+    }
+});
+
+export default store;
